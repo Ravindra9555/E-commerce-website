09cbc6cb36bd44715155e4a9b3cb3cd506310c54
Ravindra Kumar <ravindrakar9658@gmail.com>
2022-06-28 12:10:52 +0530

    under_development

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
- let bar1=document.getElementById('bar1');
-const close=document.getElementById('close');
-const nav =document.getElementById('navbar');
- if(bar1){
-    bar1.addEventListener('click',() =>{
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+if (bar) {
+    bar.addEventListener('click', () => {
         nav.classList.add('active');
     })
- }
- if(close){
-    close.addEventListener('click',() =>{
+}
+if (close) {
+    close.addEventListener('click', () => {
         nav.classList.remove('active');
-        
+
     })
- }
+}
